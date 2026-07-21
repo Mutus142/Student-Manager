b544452305f6ee524904499461397315d6c663f2
Mutus <mateus08142@gmail.com>
2026-07-20 21:16:19 -0300

    Ajustes

--- menu.py
+++ menu.py
@@ -1,25 +1,28 @@
-while: True
+while True:
 
-print("""
-Escolha uma Opção
+    print("""
+    Escolha uma Opção
 
-1 - Cadastrar Aluno
-2 - Adicionar Nota
-3 - Ver Boletim
-4 - Sair
-""")
+    1 - Cadastrar Aluno
+    2 - Adicionar Nota
+    3 - Ver Boletim
+    4 - Sair
+    """)
 
-escolha = int(input("Digite o numero para a proxima etapa: "))
+    escolha = int(input("Digite o numero para a proxima etapa: "))
 
-if escolha == 1:
-    cadastrar_aluno()
+    if escolha == 1:
+        cadastrar_aluno()
 
-elif escolha == 2:
-    adicionar_nota()
+    elif escolha == 2:
+        adicionar_nota()
 
-elif escolha == 3:
-    ver_boletim()
+    elif escolha == 3:
+        ver_boletim()
 
-else:
-    print("Saindo...")
-    break
+    elif escolha == 4:
+        print("Saindo...")
+        break
+
+    else:
+        print("Operação Inválida!")
